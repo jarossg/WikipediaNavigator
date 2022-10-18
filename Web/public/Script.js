e87ead73d7f1df48bf1bee3224821eb58a7f2d6f
jarossg <g-jaross@web.de>
2022-10-18 20:59:44 +0200

    Out of Memory

--- Web/public/Script.js
+++ Web/public/Script.js
@@ -6,35 +6,6 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
-
-//var data = httpGet("https://wikipedianavigator-default-rtdb.europe-west1.firebasedatabase.app/articles.json");
-//console.log(data);
-
-
-function start(){
-  var nodes = new vis.DataSet([
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" },
-  ]);
-
-  nodes.add({id: 6, label: "Knoten"});
-
-  // create an array with edges
-  var edges = new vis.DataSet([
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-    { from: 3, to: 3 },
-  ]);
-
-  buildGraph(nodes, edges);
-}
-
-
 function buildGraph(nodes, edges){
   // create a network
   var container = document.getElementById("network");
@@ -45,4 +16,20 @@ function buildGraph(nodes, edges){
 
   var options = {};
   var network = new vis.Network(container, data, options);
+}
+
+function start(){
+  var nodes = new vis.DataSet();
+  var edges = new vis.DataSet();
+  
+  var data = httpGet("https://wikipedianavigator-default-rtdb.europe-west1.firebasedatabase.app/articles.json");
+  console.log("Data aquired");
+  var index = 0;
+
+  for(var key in Object.keys(data)){
+    nodes.add({id: index, label: key});
+    index = index + 1;
+  }
+  
+  buildGraph(nodes, edges);
 }
