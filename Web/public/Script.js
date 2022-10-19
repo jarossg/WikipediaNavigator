a8a3190fd41b6846ecd9e1ed13467743b77c5ac8
jarossg <g-jaross@web.de>
2022-10-19 14:06:47 +0200

    incremental load

--- Web/public/Script.js
+++ Web/public/Script.js
@@ -22,14 +22,18 @@ function start(){
   var nodes = new vis.DataSet();
   var edges = new vis.DataSet();
   
-  var data = httpGet("https://wikipedianavigator-default-rtdb.europe-west1.firebasedatabase.app/articles.json");
+  var data = httpGet("https://wikipedianavigator-default-rtdb.europe-west1.firebasedatabase.app/.json");
   console.log("Data aquired");
+  console.log(data);
   var index = 0;
 
-  for(var key in Object.keys(data)){
-    nodes.add({id: index, label: key});
+  for(let key in data){
+    if(index > 5){break;}
+    console.log(key);
+    nodes.add({id: index, label: key["title"]});
     index = index + 1;
+
   }
-  
+  console.log("building Graph " + index);
   buildGraph(nodes, edges);
 }
