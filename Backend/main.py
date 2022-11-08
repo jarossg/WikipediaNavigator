d13336fdd0db6c7c1a963e6b6eaddd7a20ee89ee
jarossg <g-jaross@web.de>
2022-11-08 12:19:10 +0100

    finished Backend

--- Backend/main.py
+++ Backend/main.py
@@ -14,6 +14,9 @@ def main(article):
         articles = {"articles": []}
 
     articles["articles"][article] = request.json
+
+    with open('/data/result.json', 'w') as f:
+        json.dump(articles, f)
     return Response(status=200)
 
 @app.route("/get",methods=["GET"])
@@ -22,8 +25,8 @@ def get():
         with open('/data/result.json', 'r') as f:
             articles = json.load(f)
     except FileNotFoundError:
-        articles = {"articles": []}
+        articles = {"articles": {}}
 
     return Response(status=200, data=articles)
 
-app.run(port=5000)
+app.run(port=5000, host="0.0.0.0")
