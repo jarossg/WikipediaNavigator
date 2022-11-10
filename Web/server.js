b00cc9736afbdf176af926a94d2cbe59e31ffb80
jarossg <g-jaross@web.de>
2022-11-10 21:07:21 +0100

    server.js fixed

--- Web/server.js
+++ Web/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'/public/index.html'));
+  res.sendFile('./public/index.html');
 });
 
 app.listen(PORT, HOST, () => {
