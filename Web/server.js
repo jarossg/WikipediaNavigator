979b5a96a306a85115c5ebd7b36408ea3c6de39f
jarossg <g-jaross@web.de>
2022-11-10 21:17:29 +0100

    Commitnachricht die stark aufragt

--- Web/server.js
+++ Web/server.js
@@ -14,7 +14,13 @@ app.get('/', (req, res) => {
 app.get('/Style.css', (req, res) => {
     res.sendFile('/usr/src/app/public/Style.css');
   });
-  
+
+  app.get('/Script.js', (req, res) => {
+    res.sendFile('/usr/src/app/public/Script.js');
+  });
+  app.get('/Graph.js', (req, res) => {
+    res.sendFile('/usr/src/app/public/Graph.js');
+  });
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
